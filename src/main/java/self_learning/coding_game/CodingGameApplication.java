52cf95f00e6c56c9bb7356e8bce798fc6ce84759
fazilshafibhat <fazilshafi.fsb382@gmail.com>
2024-02-07 12:18:47 +0530

    Project setup

---
+++ src/main/java/self_learning/coding_game/CodingGameApplication.java
@@ -0,0 +1,14 @@
+package self_learning.coding_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingGameApplication.class, args);
+	
+	}
+
+}
---
+++ src/test/java/self_learning/coding_game/CodingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package self_learning.coding_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
